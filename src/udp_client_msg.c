f1437580265cf42ba38d80b3bb9aea4907e3b39b
Zachary Kurt Boronka <zboronka@gmail.com>
2020-03-01 14:16:37 -0500

    UDP timed by message size client/server started

--- src/udp_client_msg.c
+++ src/udp_client_msg.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <errno.h>
 
 #define BUFLEN 2048
 #define PORT   2830
@@ -17,11 +18,18 @@ void die(char *s) {
 }
 
 int main(int argc, char **argv) {
-	int bts[] = {8, 64, 1024};
+	int bts[] = {1024, 512, 256};
+	int tts[] = {1024, 2048, 4096};
 	struct hostent *hp;
 	struct sockaddr_in si_other;
 	int s, i, slen=sizeof(si_other);
+
+	struct timeval timeout;
+	timeout.tv_sec = 2;
+	timeout.tv_usec = 0;
+
 	char buf[BUFLEN];
+	char drop[] = "drop";
 	char message[BUFLEN];
 	char *host, sep;
 	clock_t send_t, reply_t;
@@ -42,12 +50,16 @@ int main(int argc, char **argv) {
 		die("socket");
 	}
 
+	if(setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+		die("setsockopt");
+	}
+
 	memset((char *) &si_other, 0, sizeof(si_other));
 	si_other.sin_family = AF_INET;
 	si_other.sin_port = htons(PORT);
 	bcopy(hp->h_addr, (char*)&si_other.sin_addr, hp->h_length);
 	
-	printf("8B,64B,1024B\n");
+	printf("1024B,512B,256B\n");
 
 	for(int i = 0; i < 50; i++) {
 		for(int j = 0; j < 3; j++) {
@@ -55,12 +67,22 @@ int main(int argc, char **argv) {
 			memset(message, 'p', bts[j] * sizeof(char));
 			message[bts[j]] = '\0';
 			send_t = clock();
-			if(sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == -1) {
-				die("sendto");
+			for(int k = 0; k < tts[j]; k++) {
+				if(sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == -1) {
+					die("sendto");
+				}
 			}
-			bzero(buf, BUFLEN);
+
 			if(recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1) {
-				die("recvfrom");
+				if(errno == EAGAIN) {
+					if(sendto(s, drop, strlen(drop), 0, (struct sockaddr *) &si_other, slen) == -1) {
+						die("sendto");
+					}
+					j--;
+					continue;
+				} else {
+					die("recvfrom");
+				}
 			}
 			reply_t = clock();
 			printf("%f%c", ((double) (reply_t - send_t)) / CLOCKS_PER_SEC, sep);
---
+++ src/udp_server_msg.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFLEN 2048
+#define PORT   2830
+
+void die(char *s) {
+	perror(s);
+	exit(1);
+}
+
+int main() {
+	struct sockaddr_in si_me, si_other;
+
+	int s, i, slen=sizeof(si_other), recv_len;
+	char buf[BUFLEN];
+	char reply[] = "ACK";
+	int total_rec = 0;
+
+	if((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+		die("socket");
+	}
+
+	memset((char *) &si_me, 0, sizeof(si_me));
+
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(PORT);
+	si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	if(bind(s, (struct sockaddr*)&si_me, sizeof(si_me)) == -1) {
+		die("bind");
+	}
+
+	while(1) {
+		fflush(stdout);
+
+		if((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) {
+			die("recvfrom");
+		}
+
+		total_rec+=recv_len;
+
+		//printf("Received size %dB packet from %s:%d\n", recv_len, inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+		
+		if(total_rec == 1024*1024) {
+			if(sendto(s, reply, sizeof(reply), 0, (struct sockaddr *) &si_other, slen) == -1) {
+				die("sendto");
+			}
+			total_rec = 0;
+		}
+
+		if(strstr(buf, "drop")) {
+			printf("Dropped packets with %dB receieved\n", total_rec);
+			total_rec = 0;
+		}
+	}
+}

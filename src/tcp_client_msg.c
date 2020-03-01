c5dd6af9253c28900f9019fb74c726578ffc065d
Zachary Kurt Boronka <zboronka@gmail.com>
2020-02-29 23:58:47 -0500

    Cannae fucking know what tho fuck

---
+++ src/tcp_client_msg.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define BUFLEN 1040
+#define PORT   2830
+
+int main(int argc, char** argv) {
+	int bts[] = {1024, 512, 256};
+	int tts[] = {1024, 2048, 4096};
+	struct hostent *hp;
+	struct sockaddr_in sin;
+	char *host;
+	char buf[BUFLEN];
+	char rec_buf[16];
+	int s;
+	int rec_len;
+	clock_t send_t, reply_t;
+	char sep;
+
+	if(argc>=2) {
+		host = argv[1];
+	} else {
+		host = "localhost";
+	}
+
+	hp = gethostbyname(host);
+	if(!hp) {
+		fprintf(stderr, "unknown host: %s\n", host);
+		exit(1);
+	}
+
+	bzero((char*)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	bcopy(hp->h_addr, (char*)&sin.sin_addr, hp->h_length);
+	sin.sin_port = htons(PORT);
+
+	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(1);
+	}
+	if(connect(s, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
+		perror("connect");
+		close(s);
+		exit(1);
+	}
+
+
+	printf("1024B,512B,256B\n");
+
+	//for(int i = 0; i < 50; i++) {
+		//for(int j = 0; j < 3; j++) {
+		for(int j = 0; j < 1; j++) {
+			sep = j < 2 ? ',' : '\n';
+			memset(buf, 'p', bts[j] * sizeof(char));
+			send_t = clock();
+			for(int k = 0; k < tts[j]-1; k++) {
+				if(send(s, buf, strlen(buf), 0) < 0) {
+					perror("send");
+					close(s);
+					exit(1);
+				}
+			}
+			strcpy(buf+((bts[j])-4), "\r\n\r\n");
+			if(send(s, buf, strlen(buf), 0) < 0) {
+				perror("send");
+				close(s);
+				exit(1);
+			}
+			if(rec_len = recv(s, rec_buf, sizeof(rec_buf), 0)) {
+				if(rec_len < 0) {
+					perror("recv");
+					break;
+				}
+				reply_t = clock();
+				printf("%f%c", ((double) (reply_t - send_t)) / CLOCKS_PER_SEC, sep);
+			}
+		}
+	//}
+
+	shutdown(s, SHUT_RDWR);
+}
---
+++ src/tcp_server_msg.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFLEN 1000256
+#define PORT   2830
+#define MAX_PENDING  5
+
+int main() {
+	struct sockaddr_in sin;
+	char *buf = malloc(BUFLEN * sizeof(char));
+	int buf_len, addr_len;
+	int s, new_s, opt = 1;
+	int offset = 0;
+
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = INADDR_ANY;
+	sin.sin_port = htons(PORT);
+
+	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(1);
+	}
+
+	if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+		perror("setsockopt"); 
+		exit(1);
+	}
+
+	if((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+		perror("bind");
+		exit(1);
+	}
+
+	if(listen(s, MAX_PENDING) < 0) {
+		perror("listen");
+		exit(1);
+	}
+
+	while(1) {
+		if((new_s = accept(s, (struct sockaddr *)&sin, &addr_len)) < 0) {
+			perror("accept");
+			exit(1);
+		}
+		while(buf_len = recv(new_s, buf+offset, (BUFLEN-offset) * sizeof(char), 0)) {
+			if(!strstr(buf, "\r\n\r\n")) {
+				if(buf_len < 0) {
+					perror("recv");
+					break;
+				}
+				//printf("Received %dB\n", buf_len);
+				offset+=buf_len;
+				continue;
+			}
+
+			printf("Total %dB\n", strlen(buf));
+			send(new_s, "ACKNWLGE", 8, MSG_NOSIGNAL);
+			offset=0;
+			bzero(buf, BUFLEN * sizeof(char));
+		}
+		close(new_s);
+	}
+
+	free(buf);
+}
